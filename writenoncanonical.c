abf63f3d01f2453ddf212ecd11007c5d45faacf6
MiguelOliveira13 <165916493+MiguelOliveira13@users.noreply.github.com>
2024-05-16 12:19:47 +0100

    Update writenoncanonical.c

--- writenoncanonical.c
+++ writenoncanonical.c
@@ -22,7 +22,11 @@
 #define F 0x5c
 #define A 0x03
 #define C 0x06
+#define C_11 0x11
+#define C_01 0x01
 #define BCC A^C
+#define BCC_11 A^C_11
+#define BCC_01 A^C_01
 
 volatile int STOP=FALSE;
 
@@ -30,11 +34,14 @@ volatile int STOP=FALSE;
 void mens(int fd)
 {
 	unsigned char bufm[9];
-	int res1;
+	int res1, pos=1;
+	
 	
 	bufm[0] = 0x5c; //FLAG
 	bufm[1] = 0x03; //AD
-	bufm[2] = 0x80; //C_Dados
+	if(pos%2!=0) bufm[2] = 0x80; //C_Dados
+	if(pos%2==0) bufm[2] = 0xc0;
+	pos++;
 	
 	bufm[3] = bufm[1]^bufm[2]; //BCC1
 	bufm[4] = 0x11; //Dados
@@ -47,8 +54,95 @@ void mens(int fd)
 
     res1 = write(fd,bufm,9);
     printf("%d bytes written\n", res1);
+    
+    printf("Vai fazer a rece\n");
+    fflush(stdout);
+    
+    rec_RR(fd);
 
 	
+}
+
+void rec_RR(int fd){
+	
+	printf("Abriu a rece \n");
+	
+	unsigned char buf[255]; int estado = 0;
+	
+	while(estado != STOP_){
+        read(fd, buf, 1);
+        printf("%X ", buf[0]);
+        switch (estado)
+        {
+        case START:
+            if(buf[0] == F){
+                estado = FLAG_RCV;
+            }
+            else{
+                estado = START;
+            }
+            break;
+        case FLAG_RCV:
+            if(buf[0] == A){
+                estado = A_RCV;
+            }
+            else if(buf[0]==F){
+                estado = FLAG_RCV;
+            }
+            else{
+                estado = START;
+            }
+            break;
+        case A_RCV:
+            if(buf[0] == C_11){
+                estado = C_RCV;
+            }
+            else if(buf[0]== C_01){
+                estado = C_RCV;
+            }
+            else if(buf[0]==F){
+                estado = FLAG_RCV;
+            }
+            else{
+                estado = START;
+            }
+            break;
+        case C_RCV:
+            if(buf[0] == BCC_11 || buf[0] == BCC_01){
+                estado = BCC_OK;
+            }
+            else if(buf[0]==F){
+                estado = FLAG_RCV;
+            }
+            else{
+                estado = START;
+            }
+            break;
+        case BCC_OK:
+            if(buf[0] == F){
+                estado = STOP_;
+            }
+            else if(buf[0]==F){
+                estado = FLAG_RCV;
+            }
+            else{
+                estado = START;
+            }
+            break;
+        case STOP_:
+            estado = STOP_;
+            printf("STOP atingido.\n");
+            break;
+        default:
+            printf("Default ativado, algo está incorreto.\n");
+            break;
+        }
+
+    }
+	printf("\n");
+	printf("STOP atingido.\n");
+	printf("Recebida a Confirmação\n");
+
 }
 
 
