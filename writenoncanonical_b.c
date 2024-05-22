717652564ec5e38701eb7d57368a42c8a21db5e1
MiguelOliveira13 <165916493+MiguelOliveira13@users.noreply.github.com>
2024-05-22 15:07:59 +0100

    Update writenoncanonical_b.c

--- writenoncanonical_b.c
+++ writenoncanonical_b.c
@@ -51,7 +51,7 @@ typedef struct linkLayer{
 } linkLayer;
 
 volatile int STOP=FALSE;
-unsigned char bufw[4], buf[255];
+unsigned char bufw[6], buf[255];
 int fd;
 
 void UA_W()
@@ -61,14 +61,11 @@ void UA_W()
     bufw[0] = 0x5c; //FLAG
 	bufw[1] = 0x03; //A
 	bufw[2] = 0x06; //C_UA
-	bufw[3] = 0x04; //BCC
+	bufw[3] = 0x05; //BCC
 	bufw[4] = 0x5c; //F
-  
-
-    /*testing*/
     bufw[5] = '\n';
 
-    res = write(fd,bufw,5);
+    res = write(fd,bufw,7);
     printf("%d bytes written\n", res);
 }
 
@@ -77,15 +74,19 @@ void connect()
     printf("Entrou connect\n");
     
     int res;
+
+    printf("Vai escrever no buffer\n");
     
     bufw[0] = 0x5c; //FLAG
 	bufw[1] = 0x03; //A
 	bufw[2] = 0x07; //C
-	bufw[3] = buf[1]^buf[2]; //BCC
+	bufw[3] = bufw[1]^bufw[2]; //BCC
 	bufw[4] = 0x5c; //F
     bufw[5] = '\n';
 
-    res = write(fd,bufw,5);
+    printf("Vai escrever o write\n");
+
+    res = write(fd,bufw,7);
     printf("%d bytes written\n", res);
 }
 
@@ -292,10 +293,39 @@ int llopen_W(struct linkLayer ll)
     printf("New termios structure set WRITE\n");
 
     int estado =0;
+    struct termios oldtio,newtio;
 
     fd = open(ll.serialPort, O_RDWR | O_NOCTTY );
-
     if(fd<0){printf("Error\n");return-1;}
+
+    if (tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    /* set input mode (non-canonical, no echo,...) */
+    newtio.c_lflag = 0;
+
+    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+
+    /*
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
+    leitura do(s) próximo(s) caracter(es)
+    */
+
+
+    tcflush(fd, TCIOFLUSH);
+
+    if (tcsetattr(fd,TCSANOW,&newtio) == -1) {
+        perror("tcsetattr");
+        exit(-1);
+    }
 
     printf("Vai entrar no connect\n");
     connect(fd);
